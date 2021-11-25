0fdaacdc0b94de0b687b63935d40f0fe25292af1
Iryna Chystabayeva <chistobaevairina@gmail.com>
2021-11-25 19:49:23 +0300

    Add task 1 hw 3

--- specs/message-service.js
+++ specs/message-service.js
@@ -11,45 +11,92 @@ const {
     assertResponse
 } = require('./stubs/messageservice.stubs');
 
-// REMOVE THE BELOW CODE BEFORE START THE EXERCISE
-
-function startAllNodes() {
-    startClientPC();
-    const earthToken = startEarthServer();
-    const marsToken = startMarsServer();
-    startSatelite();
-    return {
-        earth: earthToken,
-        mars: marsToken,
-    }
-}
-
-function stopAllNodes(){
-    stopMarsServer();
-    stopEarthServer();
-    stopSatelite();
-    stopClientPC();
-}
-
 describe('Message Sending', function () {
-    it('should send message to Mars without error', function () {
-        let tokens = startAllNodes();
-        const response = sendMessage('Hello', 'Mars', tokens.mars);
-        assertResponse(response, 'Success');
-        stopAllNodes()
+
+    before('Start Client PC', function () {
+        startClientPC();
     });
 
-    it('should send message to Earth without error', function () {
-        let tokens = startAllNodes();
-        const response = sendMessage('Hello', 'Earth', tokens.earth);
-        assertResponse(response, 'Success');
-        stopAllNodes()
-    });
+    context('All nodes presented:', function () {
+        beforeEach('Start Satelite', function () {
+            startSatelite();
+        });
 
-    it('should send message to Earth with "Security Error"', function () {
-        startAllNodes();
-        const response = sendMessage('Hello', 'Earth', 'X0000');
-        assertResponse(response, 'Security Error');
-        stopAllNodes()
+        it('should send message to Earth without error', function () {
+            let earthToken = startEarthServer();
+            const response = sendMessage('Hello', 'Earth', earthToken);
+            assertResponse(response, 'Success');
+        });
+
+        it('should send message to Mars without error', function () {
+            const marsToken = startMarsServer();
+            const response = sendMessage('Hello', 'Mars', marsToken);
+            assertResponse(response, 'Success');
+        });
+
+        afterEach('Stop all nodes', function () {
+            stopMarsServer();
+            stopEarthServer();
+            stopSatelite();
+        });
+    })
+
+    context('Invalid token present:', function () {
+        beforeEach('Start all nodes', function () {
+            startSatelite();
+            startEarthServer();
+            startMarsServer();
+        });
+
+        it('should not send message to Earth with invalid token', function () {
+            const earthToken = 'W1234';
+            const response = sendMessage('Hello', 'Earth', earthToken);
+            if (response !== 'Success') {
+                throw new Error('Token is invalid');
+            }
+        });
+
+        it('should not send message to Mars with invalid token', function () {
+            const marsToken = 'W1234';
+            const response = sendMessage('Hello', 'Mars', marsToken);
+            if (response !== 'Success') {
+                throw new Error('Token is invalid');
+            }
+        });
+
+        afterEach('Stop all nodes', function () {
+            stopSatelite();
+            stopMarsServer();
+            stopEarthServer();
+        });
+    })
+
+    context('Satellite disabled:', function () {
+        beforeEach('Start Mars Server', function () {
+            startMarsServer();
+        });
+
+        it('should not send message to Mars with valid token but disabled satellite', function () {
+            const marsToken = startMarsServer();
+            const response = sendMessage('Hello', 'Mars', marsToken);
+            if (response !== 'Success') {
+                throw new Error('Satellite is disabled');
+            }
+        });
+
+        it('should not send message to Mars with invalid token and disabled satellite', function () {
+            const marsToken = 'W1234';
+            const response = sendMessage('Hello', 'Mars', marsToken);
+            if (response !== 'Success') {
+                throw new Error('Token is invalid and satellite is disabled');
+            }
+        });
+
+        afterEach('Stop Mars Server', function () {
+            stopMarsServer();
+        });
+    })
+    after('Stop Client PC', function () {
+        stopClientPC();
     });
 })
